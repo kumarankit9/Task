e2d8d42e985d3149a57d00384e46d5527435a879
kumarankit9 <54777927+kumarankit9@users.noreply.github.com>
2019-12-25 21:06:57 +0530

    Add files via upload
    junit programs

---
+++ junit-basics/src/main/java/com/banglore/MathUtils.java
@@ -0,0 +1,26 @@
+package com.banglore;
+
+public class MathUtils {
+   
+	public int add(int a,int b) {
+	    return a+b;
+	}
+	
+	public int sub(int a,int b) {
+	    return a-b;
+	}
+	
+	public Integer arrayCheck(int ar[])
+	{
+		int arr[]=ar;
+		int max=0;
+		for(int i=0;i<=arr.length-1;i++ )
+		{
+			if(arr[i]>max)
+				max=arr[i];
+		}
+		return max;
+		
+	}
+	
+}
---
+++ junit-basics/src/test/java/com/banglore/MathUtilsTest.java
@@ -0,0 +1,43 @@
+package com.banglore;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MathUtilsTest {
+
+	MathUtils mathutils;
+	
+	@Before
+	public void setUp() {
+		 mathutils=new MathUtils();
+	}
+	@Test
+	public void testAdd() {
+		
+		int expected=2;
+		int actual=mathutils.add(1,1);
+		assertEquals(expected, actual);
+		System.out.println("test case ran");
+	}
+	
+	@Test
+	public void testSub() {
+		
+		int expected=0;
+		int actual=mathutils.sub(1,1);
+		assertEquals(expected, actual);
+		System.out.println("test case ran");
+	}
+	
+	@Test
+	public void testArrayCheck() {
+		
+		int expected=4;
+		int actual=mathutils.arrayCheck(new int[]{1,3,4,2});
+		assertEquals(expected, actual);
+		System.out.println("test case ran");
+	}
+
+}
